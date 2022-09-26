c33f7e7ab27b82f04ec395de68dd7474cd59b811
karina A <karina.arevalo@hotmail.com>
2022-09-26 10:28:03 -0500

    Removing extra"s"

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,15 +1,13 @@
 <script setup>
-  import MainCardsSingle from './MainCardsSingle.vue'
+  import MainCardsSingle from '@/components/MainCardsSingle.vue'
 </script>
 
 <template>
-  <div class="sub-wrapper">
-    <MainCardsSingle v-for="n in 20" />
-  </div>
+  <div class="sub-wrapper"><MainCardsSingle v-for="n in 20" :key="n" /></div>
 </template>
 
-<style scoped lang="postcss">
-  .sub-wrapper {
-    @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
-  }
-</style>
+<Style scoped lang="postcss">
+.sub-wrapper {
+  @apply grid grid-cols-1 gap-5  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5;
+}
+</Style>
