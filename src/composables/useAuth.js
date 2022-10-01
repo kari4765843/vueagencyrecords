a14329de04c3cfea0a39924a85a8b11f478c1d67
karina A <karina.arevalo@hotmail.com>
2022-10-01 15:39:23 -0500

    Adding the firebase

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,8 +1,8 @@
-import router from './router'
+import router from '@/router'
 import { firebaseApp } from '@/composables/useFirebase'
 
 import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
-import { useAuth as firebaseAuth } from '@/vueuse/firebase/useAuth'
+import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
 
 const auth = getAuth(firebaseApp)
 
