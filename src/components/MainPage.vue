c5fa79ed4a7356a6d3fdaacf3b694ddc981a516f
karina A <karina.arevalo@hotmail.com>
2022-11-26 19:08:28 -0600

    main page get employees

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,19 +1,29 @@
 <script setup>
+  import { onMounted } from 'vue'
+
   import MainSearch from '@/components/MainSearch.vue'
   import MainCards from '@/components/MainCards.vue'
   import MainPagination from '@/components/MainPagination.vue'
+  import useAPI from '@/composables/useAPI'
+
+  const { getEmployees, loading } = useAPI()
+
+  onMounted(async () => {
+    await getEmployees()
+  })
 </script>
 
 <template>
-  <div class="wrapper">
+  <p v-if="loading">Loading...</p>
+  <div v-else class="wrapper">
     <MainSearch />
     <MainCards />
     <MainPagination />
   </div>
 </template>
 
-<Style scoped lang="postcss">
-.wrapper {
-  @apply container mx-auto flex flex-col gap-4 py-4;
-}
-</Style>
+<style scoped lang="postcss">
+  .wrapper {
+    @apply container mx-auto flex flex-col gap-4 py-4;
+  }
+</style>
